4b6d4d2f482e4fec6990e5dd0ee4f0fcfddfb7a4
Bala Subrahmanyam Varanasi <bala@tinyowl.co.in>
2015-10-31 20:31:31 +0530

    define initialize and include? methods in time_window

--- lib/time_window.rb
+++ lib/time_window.rb
@@ -1,6 +1,17 @@
+require 'time'
+require 'time_range'
+
 # TimeWindow
 class TimeWindow
-  def self.hi
-    'Hello world!'
+  def initialize(time_range_strings = [])
+    @ranges = []
+    time_range_strings.each do |time_range_string|
+      @ranges << TimeRange.new(time_range_string)
+    end
+  end
+
+  def include?(time)
+    return true if @ranges.empty?
+    @ranges.any? { |x| x.include?(time) }
   end
 end
